4413199347b476067441a42b2075bb0622dd97e5
rakibcse99 <rakib.cse99@gmail.com>
2024-05-16 12:42:46 +0600

    implemention : Box in compose

--- app/src/main/java/com/example/basicjatpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/basicjatpackcompose/MainActivity.kt
@@ -4,37 +4,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicjatpackcompose.ui.theme.BasicJatpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
+        setContent {
+            MyContent()
+        }
 
     }
 }
 
-
-@Preview(showBackground = true, heightDp = 100, widthDp = 100)
-@Composable
-fun GreetingPreview() {
-    Text(text = "hello rakib")
-}
-
-
 @Preview(showBackground = true)
 @Composable
-fun Second() {
-    Text(text = "hello", fontStyle =FontStyle.Italic, color = Color.Red, textAlign = TextAlign.Center )
+fun MyContent() {
+    Column(modifier = Modifier.size(200.dp)) {
+        Image(
+            painter = painterResource(id = androidx.core.R.drawable.ic_call_answer),
+            contentDescription = "dummy",
+            modifier = Modifier.size(200.dp)
+        )
+        Icon(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "j",Modifier.size(24.dp))
+    }
 }
